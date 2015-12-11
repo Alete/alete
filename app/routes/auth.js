cf02108b5028b292a79b946e19eff915cef89410
X O <xo@wvvw.me>
2015-12-12 00:04:00 +1030

    add route checking for blogs

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -4,29 +4,53 @@ var express  = require('express'),
 module.exports = (function() {
     var app = express.Router();
 
-    app.get('/signin', function(req, res){
-        res.render('signin');
+    app.get('/signin', function(req, res, next){
+        if(!res.locals.subDomain){
+            res.render('signin');
+        } else {
+            next();
+        }
     });
 
-    app.get('/signup', function(req, res){
-        res.render('signup');
+    app.get('/signup', function(req, res, next){
+        if(!res.locals.subDomain){
+            res.render('signup');
+        } else {
+            next();
+        }
     });
 
-    app.post('/signin', passport.authenticate('local-signin', {
+    app.post('/signin', function(req, res, next){
+        if(!res.locals.subDomain){
+            next();
+        } else {
+            next('route');
+        }
+    }, passport.authenticate('local-signin', {
         successRedirect : '/', // redirect to the secure profile section
         failureRedirect : '/signin', // redirect back to the signup page if there is an error
         failureFlash : false // allow flash messages
     }));
 
-    app.post('/signup', passport.authenticate('local-signup', {
+    app.post('/signup', function(req, res, next){
+        if(!res.locals.subDomain){
+            next();
+        } else {
+            next('route');
+        }
+    }, passport.authenticate('local-signup', {
         successRedirect : '/',
         failureRedirect : '/signup',
          failureFlash : false // allow flash messages
     }));
 
-    app.get('/signout', function(req, res){
-        req.logout();
-        res.redirect('/');
+    app.get('/signout', function(req, res, next){
+        if(!res.locals.subDomain){
+            req.logout();
+            res.redirect('/');
+        } else {
+            next();
+        }
     });
 
     return app;
