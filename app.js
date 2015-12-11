843faa0982ff628a329020f2599542b43acc5b80
X O <xo@wvvw.me>
2015-12-12 00:06:49 +1030

    move locals first to fix auth

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./app/config/passport.js')(app, passport);
-app.use('/', require('./app/routes/auth')); // This handles passport's routes
 app.use('/', require('./app/routes/setupLocals')); // This sets subDomain and anything else we need in res.locals
+app.use('/', require('./app/routes/auth')); // This handles passport's routes
 app.use('/', require('./app/routes/www')); // Main site's pages eg. alete.xyz
 app.use('/', require('./app/routes/userBlog')); // Blog pages eg. xo.alete.xyz
 
