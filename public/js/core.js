7e13852b3ca406c91265a5b9fb58b7e407298252
X O <xo@wvvw.me>
2015-12-13 16:38:52 +1030

    add reflow and heart support

--- public/js/core.js
+++ public/js/core.js
@@ -1,5 +1,37 @@
 $(document).ready(function(){
-    $('.activityFeed').isotope({
-        itemSelector: '.post'
+    $('.activityFeed').imagesLoaded(function() {
+        $('.activityFeed').isotope({
+            itemSelector: '.post'
+        });
+    });
+    $('.notes').on('click', function(){
+        console.log('notes', $(this).parents('.post').data('id'));
+        return false;
+    });
+    $('.reflow').on('click', function(){
+        var id = $(this).parents('.post').data('id');
+        console.log('attempting to reflow', id);
+        $.post('/activity/reflow', { _id: id }, function(data){
+            console.log('reflowed', data);
+        });
+        if($(this).hasClass('clicked')){
+            $(this).removeClass('clicked');
+        } else {
+            $(this).addClass('clicked');
+        }
+        return false;
+    });
+    $('.heart').on('click', function(){
+        var id = $(this).parents('.post').data('id');
+        console.log('heart', id);
+        $.post('/activity/heart', { _id: id }, function(data){
+            console.log('heart posted', data);
+        });
+        if($(this).hasClass('clicked')){
+            $(this).removeClass('clicked');
+        } else {
+            $(this).addClass('clicked');
+        }
+        return false;
     });
 });
